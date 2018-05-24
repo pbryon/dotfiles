a692cde639cc21524a76e9f7f3381eda6152577b
Pieter Bryon <bryonpieter@gmail.com>
2018-05-24 14:35:22 +0200

    Add touchpad mouse detection script

---
+++ touchpad.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Get the touchpad id. The -P means perl regular expressions (for \K)
+## the -i makes it case insensitive (better portability) and the -o
+## means print only the matched portion. The \K discards anything matched
+## before it so this command will print the numeric id only.
+
+touchpad=$(xinput list | grep -iPo 'touchpad.*id=\K\d+')
+trackpoint=$(xinput list | grep -iPo 'trackpoint.*id=\K\d+')
+mouse="razer"
+
+## Run every second
+while :
+do
+    ## Disable the touchpad if there is a mouse connected
+    ## and enable it if there is none.
+    xinput list | grep -iq $mouse &&  xinput disable "$touchpad" || xinput enable "$touchpad" 
+    
+    ## Always disable trackpoint:
+    xinput disable "$trackpoint" 
+    ## wait one second to avoind spamming your CPU
+    sleep 1
+done
