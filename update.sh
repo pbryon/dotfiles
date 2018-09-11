f08e7f345ff7e2db041d9834135aea5b3563a644
Pieter Bryon <bryonpieter@gmail.com>
2018-09-11 15:31:36 +0200

    Extract create_symlink function
    Creating symlinks now includes:
    * Old file removal
    * File removal confirmation
    * Symlink destination checking
    
    Also added an $arrow variable, for adjustable output styling

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ if [ ! -d "web" -o ! -d ".vim" ]; then
     exit 1
 fi
 
-if [ $1 = "all" ]; then
+if [ "$1" = "all" ]; then
     all=true
 elif [[ $1 =~ bash ]]; then
     bash=true
@@ -26,22 +26,69 @@ else
     exit 1
 fi
 
+arrow="-->"
+
+create_symlink() {
+    if [ ! "$1" ]; then
+        echo "$script: error creating symlink '$1'"
+        exit 1
+    fi
+    home_file="${HOME}/${1}"
+    repo_file="${PWD}/${1}"
+    confirm=false
+    
+    if [ -L $home_file ]; then
+        target=$(readlink $home_file)
+        if [ "$target" = "$repo_file" ]; then
+            echo "${arrow} already up to date: ${home_file}"
+            return
+        fi
+
+        echo "$home_file is a link but points to $target"
+        confirm=true
+    elif [ -f $home_file ]; then
+        echo "$home_file exists"
+        confirm=true
+    fi
+
+    if [ "$confirm" = true ]; then
+        read -p "Replace [yn]? " -n 1 reply
+        echo
+        if [ "$reply" != "y" ]; then
+            return
+        fi
+    fi
+
+    if [[ -e $home_file && ! -L $home_file ]]; then
+        rm "$home_file"
+    fi
+
+    ln -s $PWD/$1 ~/$1
+    if [ !"$!" ]; then
+        echo "$arrow created symlink: ~/$1 -> $PWD/$1"
+    fi
+}
+
 if [ "$all" -o "$bash" ]; then
+    echo
     echo Updating bash...
     cp .bashrc.conf ~
-    rm ~/.bashrc
-    ln -s $PWD/.bashrc ~/.bashrc
-    echo Reload your .bashrc
+    create_symlink ".bashrc"
+    echo "$arrow please reload your .bashrc"
 fi
 
 if [ "$all" -o "$git" ]; then
+    echo
     echo Updating git...
-    cp .gitconfig ~
-    cp .gitignore ~
+    create_symlink ".gitconfig"
+    create_symlink ".gitignore"
 fi
 
 if [ "$all" -o "$vim" ]; then
+    echo
     echo Updating vim...
-    cp .vimrc ~
+    create_symlink ".vimrc"
+    echo "$arrow copying .vim directory..."
     cp -r .vim ~
 fi
+echo
