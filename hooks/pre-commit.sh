3d7ad81af3cc6c46a462e7d31a11bc3fc884bced
Pieter Bryon <bryonpieter@gmail.com>
2018-11-30 15:16:40 +0100

    Add dotnet build pre-commit hook

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# dotnet build pre-commit hook
+extensions="\.(cs.?|sln)$"
+files=$(git diff-index --cached --name-only --diff-filter=ACMR HEAD | grep -P $extensions)
+if [ ${#files} -eq 0 ]; then
+    exit 0
+fi
+
+script=$(basename $0)
+error="$script: dotnet CLI not installed. See https://github.com/dotnet/cli/blob/master/README.md"
+command -v dotnet >/dev/null || (echo $error >&2 && exit 1)
+which dotnet >/dev/null || (echo $error >&2 && exit 1)
+dotnet --help >/dev/null || exit 1
+
+project="Nomen.sln" # adjust to your project structure
+dotnet build --verbosity quiet $project
