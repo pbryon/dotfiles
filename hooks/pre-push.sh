59217ba74950b0363e55881c6f6e0c5317c1d607
Pieter Bryon <bryonpieter@gmail.com>
2018-12-05 13:19:17 +0100

    Improve pre-push: add npm version checking

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-# pre-push npm audit hook
-frontend="src/Nomen.Presentation.Web" # replace as needed
+frontend="src/Nomen.Presentation.Web"
 echo "> Running npm audit..."
-if [ $frontend ]; then
-    cd $frontend
+cd $frontend
+
+error="npm not installed. Please install it first"
+command -v npm >/dev/null || (echo $error >&2 && exit 1)
+which npm >/dev/null || (echo $error >&2 && exit 1)
+
+version=`npm --version`
+if [[ $version != 6* ]]; then
+    echo -e "--> npm version ($version) not high enough for audit"
+    echo "    Upgrade it to at least 6.0.0 with:"
+    echo "      npm install -g npm"
+    exit 1
 fi
-npm audit >/dev/null 2>&1 || exit 1
+
+npm audit >/dev/null || exit 1
 echo "--> OK"
 exit 0
