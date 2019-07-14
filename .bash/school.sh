b707407880a48a89e4c340c77cf5f815302961f9
Pieter Bryon <bryonpieter@gmail.com>
2019-07-14 16:23:55 +0200

    Make school.sh safer to import
    Will no longer import anything if $SCHOOL isn't set

--- .bash/school.sh
+++ .bash/school.sh
@@ -1,5 +1,7 @@
-alias school="cd ${SCHOOL:-$PWD}"
-if [ "$SCHOOL" != "$PWD" ]; then
-    alias todo="cat $REPO/kdg/TODO.md | grep -v x | head -n 20"
-    alias schedule="cat $REPO/kdg/P3.md | head -n 11"
+if [ -z "$SCHOOL" ]; then
+    return
 fi
+
+alias school="cd ${SCHOOL:-$PWD}"
+alias todo="cat $REPO/kdg/TODO.md | grep -v x | head -n 20"
+alias schedule="cat $REPO/kdg/P3.md | head -n 11"
