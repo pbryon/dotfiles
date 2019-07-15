42ca7dc75c8c4740497c47e82b543c57b66ef7b5
Pieter Bryon <bryonpieter@gmail.com>
2019-07-15 15:26:05 +0200

    Bugfix - use relative source file path

--- vscode/update-extensions.sh
+++ vscode/update-extensions.sh
@@ -5,18 +5,20 @@ error="$script: VS Code not installed. https://code.visualstudio.com/download"
 
 command -v code >/dev/null || (echo $error >&2 && exit 1)
 which code >/dev/null || (echo $error >&2 && exit 1)
-code --help >/dev/null || (echo $error >&2 && exit 1)
 
 file="vscode-extensions.txt"
-
+path=dirname "$BASH_SOURCE"
+full_file="$path/$file"
+echo File: $full_file
+exit
 echo Updating VSCode extensions...
 OLD_IFS="$IFS"
 IFS=$'\n'
-if [ ! -e "$file" ]; then
+if [ ! -e "$full_file" ]; then
     echo $script: extension list missing: $file!
     return 1
 fi
-for extension in `cat $file`; do
+for extension in `cat $full_file`; do
     code --install-extension $extension --force
 done
 IFS=${OLD_IFS}
