11085f5de9631e225e23c3eee40862119c1e173f
Pieter Bryon <bryonpieter@gmail.com>
2019-07-22 23:40:18 +0200

    Bugfix - make sure script exits

--- vscode/update-extensions.sh
+++ vscode/update-extensions.sh
@@ -3,8 +3,8 @@
 script=$(basename $0)
 error="$script: VS Code not installed. https://code.visualstudio.com/download"
 
-command -v code >/dev/null || (echo $error >&2 && exit 1)
-which code >/dev/null || (echo $error >&2 && exit 1)
+command -v code >/dev/null || echo $error >&2 && exit 1
+which code >/dev/null || echo $error >&2 && exit 1
 
 file="vscode-extensions.txt"
 path=dirname "$BASH_SOURCE"
