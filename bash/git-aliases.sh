e59d698aa5fe6418fc36084478a5cc6ca53b60ca
Pieter Bryon <bryonpieter@gmail.com>
2019-10-09 11:02:59 +0200

    Add git add --interactive alias

--- bash/git-aliases.sh
+++ bash/git-aliases.sh
@@ -2,6 +2,7 @@
 alias changes="git status -vv | less -r"
 alias gaa="git add --all"
 alias gac="git add --all && git commit -v"
+alias gai="git add --interactive"
 alias gb="git branch -avv"
 alias gcp="git cherry-pick"
 alias gs="git status"
