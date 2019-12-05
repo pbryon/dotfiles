2d60410aee49be9b965153b3f6bacf8cca4dc7c0
Pieter Bryon <bryonpieter@gmail.com>
2019-12-05 12:01:22 +0100

    Document formats

--- bash/git-log.sh
+++ bash/git-log.sh
@@ -3,7 +3,21 @@ git_log () {
 }
 
 git_pretty_log() {
-    git log -n "${1:-200}" --pretty=format:"%C(dim green)%h%C(reset)     %<(20,trunc)%C(dim yellow)%an%C(reset)    %C(dim white)%s%C(reset)"
+    local beige="%C(#e6cc85)"
+    local yellow="%C(#c9b620)"
+    local light_blue="%C(#14d1db)"
+    local reset_color="%C(reset)"
+    local dim_white="%C(dim white)"
+
+    local twenty_chars="%<(20,trunc)"
+    local thirty_chars="%<(30,trunc)"
+
+    local commit_hash="%h"
+    local author_name="%an"
+    local commit_date="%ci"
+    local commit_message="%s"
+    local format="${beige}${commit_hash}${reset_color}     ${twenty_chars}${yellow}${author_name}${reset_color}    ${thirty_chars}${light_blue}${commit_date}${reset_color}    ${dim_white}${commit_message}${reset_color}"
+    git log -n "${1:-200}" --pretty=format:"$format"
 }
 
 git_reflog() {
