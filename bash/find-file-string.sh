7b035a1c0222d940387e2c1f5b4dffdae625b439
Pieter Bryon <bryonpieter@gmail.com>
2020-01-06 13:16:04 +0100

    Add find_file_string

---
+++ bash/find-file-string.sh
@@ -0,0 +1,40 @@
+file_find="ff"
+find_file_string () {
+    if [ -z "$1" ]; then
+        echo Usage:
+        echo "$file_find [extension] <pattern>"
+        echo
+        echo With no extension, it will search all files.
+        echo
+        echo "If the pattern ends with a bang (!), it's case sensitive"
+        echo
+        return 1
+    fi
+
+    extension=$1
+    pattern=$2
+    if [ -z "$pattern" ]; then
+        extension=""
+        pattern=$1
+    fi
+
+    ignore_case="i"
+    if [[ "$pattern" =~ !$ ]]; then
+        # remove bang
+        pattern=${pattern%!}
+        ignore_case=""
+    fi
+
+    directory_name="%h"
+    filename="%f"
+    format="'${PWD}'/'$directory_name'/'$filename'\n"
+    if [ -z "$extension" ]; then
+        # grep -C = context
+        find ./ -type f -printf $format | xargs grep -"$ignore_case"n "$pattern" -C 2 2>/dev/null
+        return 0
+    fi
+    
+     find ./ -type f -printf $format | grep "$extension'$" | xargs grep -"$ignore_case"n "$pattern" -C 2 2>/dev/null
+}
+
+alias $file_find=find_file_string
