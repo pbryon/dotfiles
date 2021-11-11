7f6a171ae255267cf1216b7c371b67dfb12ecd20
Pieter Bryon <bryonpieter@gmail.com>
2021-11-11 12:06:16 +0100

    Allow modifying context for gf

--- bash/find-git-string.sh
+++ bash/find-git-string.sh
@@ -2,25 +2,42 @@ git_find="gf"
 git_find_conflict="gfc"
 
 find_git_string () {
+    default_context="2"
     if [ -z "$1" ]; then
         echo Usage:
-        echo "$git_find [extension] <pattern>"
+        echo "$git_find [extension] <pattern> <context>"
         echo
         echo With no extension, it will search all files.
         echo
         echo "If the extension ends with a bang (!), it is excluded instead"
         echo "If the pattern ends with a bang (!), it's case sensitive"
         echo
+        echo The default context is $default_context lines before/after a match
+        echo
         return 1
     fi
 
+    arg_count=$#
     extension=$1
     pattern=$2
-    if [ -z "$pattern" ]; then
+    context=${3:-$default_context}
+
+    if [ $arg_count = 1 ]; then
         extension=""
         pattern=$1
+        context=$default_context
+    elif [ $arg_count = 2 ]; then
+        if [[ "$2" =~ ^[0-9]+$ ]]; then      
+            extension=""
+            pattern=$1
+            context=${2:-$default_context}
+        else
+            extension=$1
+            pattern=$2
+            context=$default_context
+        fi
     fi
-
+     
     ignore_case="i"
     if [[ "$pattern" =~ !$ ]]; then
         # remove bang
@@ -32,7 +49,7 @@ find_git_string () {
         # grep -C = context
         git ls-files \
         | grep -v -E $FIND_FILE_IGNORE \
-        | xargs grep -"$ignore_case"n "$pattern" -C 2 --color=always 2>/dev/null
+        | xargs grep -"$ignore_case"n "$pattern" -C $context --color=always 2>/dev/null
         return 0
     fi
 
@@ -43,13 +60,13 @@ find_git_string () {
         git ls-files \
         | grep -v -E $FIND_FILE_IGNORE \
         | grep -v "$extension$" \
-        | xargs grep -"$ignore_case"n "$pattern" -C 2 --color=always 2>/dev/null \
+        | xargs grep -"$ignore_case"n "$pattern" -C $context --color=always 2>/dev/null \
         | grep -v "Binary file"
     else
         git ls-files \
         | grep -v -E $FIND_FILE_IGNORE \
         | grep "${extension}$" \
-        | xargs grep -"$ignore_case"n "$pattern" -C 2 --color=always 2>/dev/null \
+        | xargs grep -"$ignore_case"n "$pattern" -C $context --color=always 2>/dev/null \
         | grep -v "Binary file"
     fi
 }
